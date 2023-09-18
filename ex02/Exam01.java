47b06e9be1b247c64e9a4ac33441636c08dd975d
SEUNGHOONBAEK <128911668+k-codingmachine@users.noreply.github.com>
2023-09-18 16:25:12 +0900

    java
    java

---
+++ ex02/Exam01.java
@@ -0,0 +1,20 @@
+package ex02;
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+		int num = 10;
+		
+		int n1;
+		int n2;
+		num = num+1;
+		
+		num+= +1;
+		
+		num++; // 후위연산자 자기가 먼저 값을 갖고 +한값을 보내준다.
+		++num; // 전위연산자
+
+		
+		
+	}
+}
