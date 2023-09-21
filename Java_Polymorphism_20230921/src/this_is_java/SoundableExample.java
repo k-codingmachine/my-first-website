0ced5d521eb8c2f6f7f0566465b48f525924e35b
SEUNGHOONBAEK <128911668+k-codingmachine@users.noreply.github.com>
2023-09-21 22:32:47 +0900

    Update SoundableExample.java

--- Java_Polymorphism_20230921/src/this_is_java/SoundableExample.java
+++ Java_Polymorphism_20230921/src/this_is_java/SoundableExample.java
@@ -4,6 +4,7 @@ public class SoundableExample {
 	
 	public static void printSound(Soundable soundable) {
 		System.out.println(soundable.sound());
+		
 	}
 	
 	public static void main(String[] args) {
