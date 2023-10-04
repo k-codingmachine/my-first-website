72c6286647b1d028ad14f595c4b0436f083e3a23
SEUNGHOONBAEK <128911668+k-codingmachine@users.noreply.github.com>
2023-10-04 23:15:58 +0900

    Update ForeachExam.java

--- Java_20231004_Ramda/src/ex08/ForeachExam.java
+++ Java_20231004_Ramda/src/ex08/ForeachExam.java
@@ -6,7 +6,9 @@ import java.util.List;
 public class ForeachExam {
 
 	public static void main(String[] args) {
+		
 		List<String> lists = Arrays.asList("Box", "Toy", "Robot", "Toy", "Box");
+
 		
 		for(String list : lists)
 			System.out.println(list);
