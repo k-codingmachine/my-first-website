291cd3fab8504298d1420f37c60063e1d8481802
SEUNGHOONBAEK <128911668+k-codingmachine@users.noreply.github.com>
2023-10-08 00:23:43 +0900

    Update hr_01.sql

--- oracle/hr_01.sql
+++ oracle/hr_01.sql
@@ -2,6 +2,7 @@ select city, country_id
 from locations
 order by country_id asc, city desc;  // 1차 조건은 오름차순 2차조건은 내림차순(생략시 asc)
 
+  
 select first_name, last_name
 from employees
 order by first_name;
@@ -22,3 +23,5 @@ where last_name = 'woo' or last_name = 'King';
 select * 
 from employees
 where employee_id = 100;
+
+// 간단한 테스트 문제
